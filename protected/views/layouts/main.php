7c648a60efa5b8f65c65320858b370796ef61d74
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-02 16:12:39 -0200

    Insercao da aba de relatorios no menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -41,6 +41,7 @@
                     array('label'=>'Venda','url'=>array('/venda')),     
                 	), 
                 ),
+				array('label'=>'Relatórios', 'url'=>array('/site/contact'), 'visible'=>!Yii::app()->user->isGuest),
                 array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest) 
             ), 
