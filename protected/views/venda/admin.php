2cd19c6b72cdd27eff9d8067a07e9040f457a18a
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-02 15:20:59 -0200

    Alteracao de acentuacao no admin de Venda

--- protected/views/venda/admin.php
+++ protected/views/venda/admin.php
@@ -29,11 +29,11 @@ $('.search-form form').submit(function(){
 <h1>Manipular Vendas</h1>
 
 <p>
-Você também pode pesquisar pelos operadores (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-ou <b>=</b>), no início de cada um dos seus valores de pesquisa para especificar como a comparação deve ser feita.
+Voc&ecirc;  tamb&eacute;m pode pesquisar pelos operadores (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+ou <b>=</b>), no in&iacute;cio de cada um dos seus valores de pesquisa para especificar como a compara&ccedil;&atilde;o deve ser feita.
 </p>
 
-<?php echo CHtml::link('Pesquisa Avançada','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Pesquisa Avan&ccedil;ada','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
