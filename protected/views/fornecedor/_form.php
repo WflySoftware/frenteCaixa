8e3d81a5cea6083a99fff367e0816ad09e4f7432
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-02 16:14:33 -0200

    Refatorcao de labels e campos na pagina _form de Fonecedor

--- protected/views/fornecedor/_form.php
+++ protected/views/fornecedor/_form.php
@@ -15,7 +15,7 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Campos <span class="required">*</span> obrigat$otilde;s.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
@@ -107,7 +107,7 @@
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton('Salvar'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
