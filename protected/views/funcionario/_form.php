af1d4091fd902e75726a07934d8f32e85550b2b3
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-02 15:56:21 -0200

    Refatoracao de labels e campos e insercao de dropDownList na camada Funcionario

--- protected/views/funcionario/_form.php
+++ protected/views/funcionario/_form.php
@@ -15,7 +15,7 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Campos <span class="required">*</span> obrigatórios.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
@@ -161,7 +161,7 @@
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton('Salvar'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
