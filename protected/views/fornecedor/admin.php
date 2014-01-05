1a1506a5ef6cf86b9b2a7d34d7595a2c529a30ab
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-05 14:54:46 -0200

    Traducao e refatoracao da _admin da camada view de Fornecedor

--- protected/views/fornecedor/admin.php
+++ protected/views/fornecedor/admin.php
@@ -29,11 +29,11 @@ $('.search-form form').submit(function(){
 <h1>Manipular Fornecedores</h1>
 
 <p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+Voc&ecirc;  tamb&eacute;m pode pesquisar pelos operadores (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+ou <b>=</b>), no in&iacute;cio de cada um dos seus valores de pesquisa para especificar como a compara&ccedil;&atilde;o deve ser feita.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Pesquisa Avan&ccedil;ada','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
