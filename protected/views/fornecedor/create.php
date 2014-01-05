69b995ab1c47177aaad9d84a9959316b3ec5c60d
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-05 14:55:56 -0200

    Traducao e refatoracao da create da camada view de Fornecedor

--- protected/views/fornecedor/create.php
+++ protected/views/fornecedor/create.php
@@ -4,7 +4,7 @@
 
 $this->breadcrumbs=array(
 	'Fornecedores'=>array('index'),
-	'Create',
+	'Cadastrar',
 );
 
 $this->menu=array(
@@ -13,6 +13,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Criar Fornecedor</h1>
+<h1>Cadastrar Fornecedor</h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
