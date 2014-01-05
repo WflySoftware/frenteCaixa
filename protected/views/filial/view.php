29b813dfcb37136bc242d8f53b99509f5074d7e0
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-05 15:25:59 -0200

    Refatoracao dos labels originais do view.php  da camada view de Filial

--- protected/views/filial/view.php
+++ protected/views/filial/view.php
@@ -3,20 +3,20 @@
 /* @var $model Filial */
 
 $this->breadcrumbs=array(
-	'Filials'=>array('index'),
+	'Filiais'=>array('index'),
 	$model->cod_filial,
 );
 
 $this->menu=array(
-	array('label'=>'List Filial', 'url'=>array('index')),
-	array('label'=>'Create Filial', 'url'=>array('create')),
-	array('label'=>'Update Filial', 'url'=>array('update', 'id'=>$model->cod_filial)),
-	array('label'=>'Delete Filial', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->cod_filial),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Filial', 'url'=>array('admin')),
+	array('label'=>'Listar Filiais', 'url'=>array('index')),
+	array('label'=>'Cadastrar Filial', 'url'=>array('create')),
+	array('label'=>'Editar Filial', 'url'=>array('update', 'id'=>$model->cod_filial)),
+	array('label'=>'Deletar Filial', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->cod_filial),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manipular Filial', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Filial #<?php echo $model->cod_filial; ?></h1>
+<h1>Ver Filial ID:<?php echo $model->cod_filial; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
