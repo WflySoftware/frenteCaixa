7c901f35f7f19c0d1722e42b9819ae8085e18703
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-05 15:19:12 -0200

    Refatoracao dos labels originais do _form da camada view de Filial

--- protected/views/filial/_form.php
+++ protected/views/filial/_form.php
@@ -15,7 +15,7 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<p class="note">Campos <span class="required">*</span> Obrigat&oacute;rios.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
@@ -44,7 +44,7 @@
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo CHtml::submitButton('Salvar'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
