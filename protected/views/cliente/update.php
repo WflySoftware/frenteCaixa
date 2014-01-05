e1d22bbc6184416978fc3917bac3606e0d17a4c4
Lucas Carvalho <lucasfilipe2007@hotmail.com>
2014-01-05 15:36:30 -0200

    Refatoracao e traducao da pagina update.php da camada view de Cliente

--- protected/views/cliente/update.php
+++ protected/views/cliente/update.php
@@ -5,17 +5,17 @@
 $this->breadcrumbs=array(
 	'Clientes'=>array('index'),
 	$model->cod_cliente=>array('view','id'=>$model->cod_cliente),
-	'Update',
+	'Editar',
 );
 
 $this->menu=array(
-	array('label'=>'List Cliente', 'url'=>array('index')),
-	array('label'=>'Create Cliente', 'url'=>array('create')),
-	array('label'=>'View Cliente', 'url'=>array('view', 'id'=>$model->cod_cliente)),
-	array('label'=>'Manage Cliente', 'url'=>array('admin')),
+	array('label'=>'Listar Clientes', 'url'=>array('index')),
+	array('label'=>'Cadastrar Cliente', 'url'=>array('create')),
+	array('label'=>'Ver Cliente', 'url'=>array('view', 'id'=>$model->cod_cliente)),
+	array('label'=>'Manipular Cliente', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Cliente <?php echo $model->cod_cliente; ?></h1>
+<h1>Editar Cliente ID:<?php echo $model->cod_cliente; ?></h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
